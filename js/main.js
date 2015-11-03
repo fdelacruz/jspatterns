ec1c5f99fe9b0c90e23d13bcffdd4c93b6183b82
Francisco De La Cruz <helloquico@gmail.com>
2015-11-03 18:54:12 -0500

    The Observer Pattern

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 require(
-		['factory/init', 'pubsub/init', 'strategy/init'],
-		function (factory, pubsub, strategy) {
-			'use strict';
+	['factory/init', 'pubsub/init', 'strategy/init', 'observer/init'],
+	function (factory, pubsub, strategy, observer) {
+		'use strict';
 
-			var examples = {
-				factory: factory,
-				pubsub: pubsub,
-				strategy: strategy
-			};
+		var examples = {
+			factory: factory,
+			pubsub: pubsub,
+			strategy: strategy,
+			observer: observer
+		};
 
-			window.runExample = function (example) {
-				examples[example].init();
-			};
-		}
+		window.runExample = function (example) {
+			examples[example].init();
+		};
+	}
 );
